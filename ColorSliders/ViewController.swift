a4f611e8d0ebd1599e917fc5c49f0782269ed7fd
Derek Grayless <grayledw@rose-hulman.edu>
2020-12-03 20:44:08 -0500

    progress up to the end of the ib-outlets and ib-actions video

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UISlider!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var alphaValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Hello world!")
+        
+        redSlider.value = 0.5
+        blueSlider.value = 0.0
+        greenSlider.value = 0.0
+        alphaSlider.value = 1.0
     }
 
-
+    @IBAction func sliderChanged(_ sender: Any) {
+        print("slider changed!")
+    }
+    
 }
 
